665057ef2d28675f8e6df9ddd738677545c4ac79
analitican <mbonnell@gmail.com>
2020-07-12 19:21:47 -0400

    Playing and learning

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const test = document.querySelectorAll("li .nav-item");
-console.log(test);
 
-fetch("https://sid7c.csb.app/menu.json")
+fetch("https://analitican.github.io/main/menu.json")
   .then(function(resp) {
     return resp.json();
   })
   .then(function(data) {
     console.log(data);
+
+const menu = data;
+
+document.getElementById('menu1').innerHTML = `${menu[0]}`;
+
+
   });
 
-// var home = data();
-
-// document.getElementById("main");
-// main.innerHTML = `${home}`;
-
-let menu = ["Home", "Pricing"];
-console.log(menu);
+//const menu = document.getElementById('menu1');
+//menu.innerText = "Home";
+//`${data[0]}`
+//document.getElementsByName('Menu2').innerHTML = `Home`;
